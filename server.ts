161be1c589172843d6a066436c610d5085cf4a2d
BrianJenney <brianjenney83@gmail.com>
2021-07-17 18:10:56 -0700

    update body limit

--- server.ts
+++ server.ts
@@ -23,7 +23,7 @@ app.use(
 );
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(allRoutes);
 
 app.listen(port, () => console.log(`Running on port ${port}`));
